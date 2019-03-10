1ed0891df7605a6627b75c6fc4c1e31afe3e2d26
TrumanGu <1227085585@qq.com>
2019-03-10 16:21:43 +0800

    refactor: 使用formdata传参，可以删去form文件夹，swagger ui的传参更直观

--- src/main/java/team/greenstudio/modules/sys/controller/SysLoginController.java
+++ src/main/java/team/greenstudio/modules/sys/controller/SysLoginController.java
@@ -5,35 +5,36 @@ import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
 import team.greenstudio.common.utils.R;
 import team.greenstudio.modules.sys.entity.SysUserEntity;
-import team.greenstudio.modules.sys.form.SysLoginForm;
 import team.greenstudio.modules.sys.service.SysUserService;
 import team.greenstudio.modules.sys.service.SysUserTokenService;
 import org.apache.shiro.crypto.hash.Sha256Hash;
 import org.springframework.beans.factory.annotation.Autowired;
+
 import java.util.Map;
 
 /**
  * @author TrumanGu trumangu1998@gmail.com
  */
 
-@Api(value="ApiValue",tags={"后台系统接口"})
+@Api(value = "ApiValue", tags = {"后台系统接口"})
 @RestController
 public class SysLoginController extends AbstractController {
+
     @Autowired
     private SysUserService sysUserService;
     @Autowired
     private SysUserTokenService sysUserTokenService;
 
 
-    @ApiOperation(value="后台用户登录",tags={"后台系统接口"},notes="")
+    @ApiOperation(value = "后台用户登录", tags = {"后台系统接口"}, notes = "")
     @PostMapping("/sys/login")
-    public Map<String, Object> login(@RequestBody SysLoginForm form){
-        SysUserEntity user = sysUserService.queryByUserName(form.getUsername());
+    public Map<String, Object> login(@RequestParam(value = "username") String username, @RequestParam(value = "password") String password) {
+        SysUserEntity user = sysUserService.queryByUserName(username);
         //账号不存在、密码错误
-        if (user == null || !user.getPassword().equals(new Sha256Hash(form.getPassword(), user.getSalt()).toHex())) {
+        if (user == null || !user.getPassword().equals(new Sha256Hash(password, user.getSalt()).toHex())) {
             return R.error("账号或密码不正确");
-        }else if (user.getStatus() == 0) {
-        //账号锁定
+        } else if (user.getStatus() == 0) {
+            //账号锁定
             return R.error("账号已被锁定,请联系管理员");
         }
         //生成token，并保存到数据库
@@ -41,8 +42,7 @@ public class SysLoginController extends AbstractController {
     }
 
 
-
-    @ApiOperation(value="后台用户注销",tags={"后台系统接口"},notes="")
+    @ApiOperation(value = "后台用户注销", tags = {"后台系统接口"}, notes = "")
     @PostMapping("/sys/logout")
     public R logout() {
         sysUserTokenService.logout(getUserId());
